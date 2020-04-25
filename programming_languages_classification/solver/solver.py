d129b91575a5de47fe1ab166c94394bd18402f31
Conti Matteo <contimatteo1505@gmail.com>
2020-04-25 15:49:51 +0200

    fixes

--- programming_languages_classification/solver/solver.py
+++ programming_languages_classification/solver/solver.py
@@ -17,12 +17,22 @@ class ProblemSolver:
         self.Networks['BAYES'] = BayesNetwork()
 
     def loadDataset(self):
+        config = {}
         self.DatasetInstance.load()
 
+        config['training'] = {}
+        config['training']['uri'] = self.DatasetInstance.TRAINING_ABS_URI
+        config['testing'] = {}
+        config['testing']['uri'] = self.DatasetInstance.TESTING_ABS_URI
+
+        self.datasetConfig = config
+
     def train(self, networkType):
+        dataset = self.datasetConfig['training']
+
         if networkType == 'SVN':
-            return self.Networks['SVN'].train()
+            return self.Networks['SVN'].train(dataset)
         elif networkType == 'CCN':
-            return self.Networks['CCN'].train()
+            return self.Networks['CCN'].train(dataset)
         else:
-            return self.Networks['BAYES'].train()
+            return self.Networks['BAYES'].train(dataset)
